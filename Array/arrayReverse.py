ab8fbd6bfd0f61d94d5785af3d825f7030be8939
pareshanjii <pj15012001@gmail.com>
2021-03-09 16:21:46 +0530

    in array

---
+++ Array/arrayReverse.py
@@ -0,0 +1,9 @@
+'''
+
+using three types we cna reverse the array :
+    first --> swaping first end with last end and first_end ++ and last_end --
+    second --> same above but recursive imp 
+    third --> slicing 
+    fourth --> by pyhton 
+
+'''
